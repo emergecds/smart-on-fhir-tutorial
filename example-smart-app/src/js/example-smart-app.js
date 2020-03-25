48db5039c41d8156c1711d9bc0c1c3db076cfc24
galexia1000 <30860165+galexia1000@users.noreply.github.com>
2020-03-25 20:24:02 +0200

    fetch demographics and observation

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -25,43 +25,43 @@
         $.when(pt, obv).fail(onError);
 
         $.when(pt, obv).done(function(patient, obv) {
-          var byCodes = smart.byCodes(obv, 'code');
-          var gender = patient.gender;
-
-          var fname = '';
-          var lname = '';
-
-          if (typeof patient.name[0] !== 'undefined') {
-            fname = patient.name[0].given.join(' ');
-            lname = patient.name[0].family.join(' ');
-          }
-
-          var height = byCodes('8302-2');
-          var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
-          var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
-          var hdl = byCodes('2085-9');
-          var ldl = byCodes('2089-1');
-
-          var p = defaultPatient();
-          p.id = patient.id;
-          p.birthdate = patient.birthDate;
-          p.gender = gender;
-          p.fname = fname;
-          p.lname = lname;
-          p.height = getQuantityValueAndUnit(height[0]);
-
-          if (typeof systolicbp != 'undefined')  {
-            p.systolicbp = systolicbp;
-          }
-
-          if (typeof diastolicbp != 'undefined') {
-            p.diastolicbp = diastolicbp;
-          }
-
-          p.hdl = getQuantityValueAndUnit(hdl[0]);
-          p.ldl = getQuantityValueAndUnit(ldl[0]);
-
-          ret.resolve(patient);
+          // var byCodes = smart.byCodes(obv, 'code');
+          // var gender = patient.gender;
+          //
+          // var fname = '';
+          // var lname = '';
+          //
+          // if (typeof patient.name[0] !== 'undefined') {
+          //   fname = patient.name[0].given.join(' ');
+          //   lname = patient.name[0].family.join(' ');
+          // }
+          //
+          // var height = byCodes('8302-2');
+          // var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
+          // var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
+          // var hdl = byCodes('2085-9');
+          // var ldl = byCodes('2089-1');
+          //
+          // var p = defaultPatient();
+          // p.id = patient.id;
+          // p.birthdate = patient.birthDate;
+          // p.gender = gender;
+          // p.fname = fname;
+          // p.lname = lname;
+          // p.height = getQuantityValueAndUnit(height[0]);
+          //
+          // if (typeof systolicbp != 'undefined')  {
+          //   p.systolicbp = systolicbp;
+          // }
+          //
+          // if (typeof diastolicbp != 'undefined') {
+          //   p.diastolicbp = diastolicbp;
+          // }
+          //
+          // p.hdl = getQuantityValueAndUnit(hdl[0]);
+          // p.ldl = getQuantityValueAndUnit(ldl[0]);
+          var p = JSON.stringify({"demographics": patient, "observation": observation})
+          ret.resolve(p);
         });
       } else {
         onError();
@@ -123,18 +123,19 @@
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
-          $('#holder').show();
-          $('#loading').hide();
+          // $('#holder').show();
+          // $('#loading').hide();
           $('#id').html(p.id);
-          $('#fname').html(p.fname);
-          $('#lname').html(p.lname);
-          $('#gender').html(p.gender);
-          $('#birthdate').html(p.birthdate);
-          $('#height').html(p.height);
-          $('#systolicbp').html(p.systolicbp);
-          $('#diastolicbp').html(p.diastolicbp);
-          $('#ldl').html(p.ldl);
-          $('#hdl').html(p.hdl);
+          // $('#fname').html(p.fname);
+          // $('#lname').html(p.lname);
+          // $('#gender').html(p.gender);
+          // $('#birthdate').html(p.birthdate);
+          // $('#height').html(p.height);
+          // $('#systolicbp').html(p.systolicbp);
+          // $('#diastolicbp').html(p.diastolicbp);
+          // $('#ldl').html(p.ldl);
+          // $('#hdl').html(p.hdl);
+
         }
     };
     var data = JSON.stringify({"demographics": p});
